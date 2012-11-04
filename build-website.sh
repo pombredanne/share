a6403b7c547c0ec65ac0105cdbd8820dfb97a6a9
Jeremy Kao <imsardine@gmail.com>
2012-11-05 01:35:58 +0800

    Wiki 靜態網站初版

---
+++ build-website.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+ASCIIDOC="asciidoc -b xhtml11 -f layout2.conf -a linkcss -a icons -a badges"
+ASCIIDOC_INDEX="$ASCIIDOC -a iconsdir=_asciidoc/images/icons -a stylesdir=_asciidoc/stylesheets -a scriptsdir=_asciidoc/javascripts"
+ASCIIDOC_ARTICLE="$ASCIIDOC -a iconsdir=../../_asciidoc/images/icons -a stylesdir=../../_asciidoc/stylesheets -a scriptsdir=../../_asciidoc/javascripts"
+
+$ASCIIDOC_INDEX index.txt 
+$ASCIIDOC_ARTICLE article/asciidoc/asciidoc.txt
+$ASCIIDOC_ARTICLE article/asciidoc/asciidoc-admonition.txt
+$ASCIIDOC_ARTICLE article/asciidoc/asciidoc-attribute.txt
+$ASCIIDOC_ARTICLE article/asciidoc/asciidoc-badge.txt
+$ASCIIDOC_ARTICLE article/asciidoc/asciidoc-callout.txt
+$ASCIIDOC_ARTICLE article/asciidoc/asciidoc-css.txt
+$ASCIIDOC_ARTICLE article/asciidoc/asciidoc-icon.txt
+$ASCIIDOC_ARTICLE article/asciidoc/asciidoc-install.txt
+$ASCIIDOC_ARTICLE article/asciidoc/asciidoc-link.txt
+$ASCIIDOC_ARTICLE article/asciidoc/asciidoc-site.txt
+$ASCIIDOC_ARTICLE article/asciidoc/asciidoc-slides.txt
+
