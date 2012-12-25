7c87cb24bf9ab819a819bfa99d3ad2339e1d85d0
Jeremy Kao <imsardine@gmail.com>
2012-12-25 23:44:53 +0800

    用 AddThis 實作 Share Buttons

---
+++ note/diagram/Diagram.mm
@@ -0,0 +1,12 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1302324788338" ID="ID_1158032778" MODIFIED="1302324791955" TEXT="Diagram">
+<node CREATED="1302324801170" ID="ID_261663421" MODIFIED="1302324803664" POSITION="right" TEXT="Tools">
+<node CREATED="1302324827107" ID="ID_1359056340" LINK="http://www.gliffy.com/" MODIFIED="1302324830789" TEXT="Gliffy"/>
+<node CREATED="1306303079428" ID="ID_128635264" LINK="http://projects.gnome.org/dia/" MODIFIED="1306303200580" TEXT="Dia"/>
+</node>
+<node CREATED="1302324843400" ID="ID_859762542" MODIFIED="1302324844461" POSITION="left" TEXT="Types">
+<node CREATED="1302324845770" ID="ID_1173524440" LINK="Flowchart.mm" MODIFIED="1302324868052" TEXT="Flowchart"/>
+</node>
+</node>
+</map>
---
+++ note/monkeyrunner/plugins/test.py
@@ -0,0 +1,2 @@
+print "without explicitly importing os.path, alias 'path' is immediately available, because of the plugin."
+print "the path separator = [%s]" % path.sep
