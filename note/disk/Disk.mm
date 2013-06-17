adb557695bbdfbe93b2de7cd9ea80a78fce786e3
Jeremy Kao <imsardine@gmail.com>
2013-06-17 10:41:30 +0800

    regular update

---
+++ note/disk/Disk.mm
@@ -0,0 +1,20 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1299421851846" ID="ID_973769863" MODIFIED="1299421856037" TEXT="Disk">
+<node CREATED="1299421859347" ID="ID_49976192" MODIFIED="1299421860628" POSITION="right" TEXT="Usage">
+<node CREATED="1299421862483" ID="ID_601939510" MODIFIED="1299421865949" TEXT="Resources">
+<node CREATED="1299421866663" ID="ID_1185528459" LINK="http://people.debian.org.tw/~chihchun/2011/02/11/cleanup-your-debian-ubuntu-disk-with-bleachbit/" MODIFIED="1299421876098" TEXT="Rex&apos;s blah blah blah &#xbb; &#x6e05;&#x7406; Debian/Ubuntu &#x5132;&#x5b58;&#x7a7a;&#x9593;"/>
+</node>
+<node CREATED="1299421880262" ID="ID_974548642" MODIFIED="1299421882050" TEXT="Tools">
+<node CREATED="1299422053458" ID="ID_637841872" MODIFIED="1299422133435" TEXT="du [Linux]">
+<font BOLD="true" NAME="Monospaced" SIZE="12"/>
+</node>
+<node CREATED="1299421976481" ID="ID_1597912813" LINK="http://www.marzocca.net/linux/baobab/" MODIFIED="1299422131526" TEXT="Baobab (Disk Usage Analyzer; Linux)">
+<font BOLD="true" NAME="Monospaced" SIZE="12"/>
+</node>
+<node CREATED="1299421949948" ID="ID_1986741032" LINK="http://www.methylblue.com/filelight/" MODIFIED="1299421959426" TEXT="Filelight (Linux/KDE)"/>
+<node CREATED="1299422337318" ID="ID_1802029095" LINK="http://bleachbit.sourceforge.net/" MODIFIED="1299422341136" TEXT="BleachBit - Clean Disk Space, Maintain Privacy"/>
+</node>
+</node>
+</node>
+</map>
